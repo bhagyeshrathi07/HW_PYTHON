afd802ab9f28706d0342f7a69d86889dd708503b
BhagyeshRathi <63847327+bhagyeshrathi07@users.noreply.github.com>
2022-03-10 21:42:07 -0800

    Add files via upload

--- timeit.py
+++ timeit.py
@@ -5,12 +5,12 @@ def calculate_time(func1):
         before_time = time.time()
         func1()
         after_time = time.time()
-        print("Function finished running in " + str(after_time - before_time) + "seconds")
+        print("Total time " + str(after_time - before_time))
     return wrapper  
 
 @calculate_time
 def printE():
     print("hey")
-    
+
 printE()
 
